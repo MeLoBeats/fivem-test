d4c59ebeaf26f303db5a107280258e4cc24d8c18
MéLoBeats <lamelobeats@gmail.com>
2025-05-16 18:09:23 +0200

    chore: fix client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,15 @@
-AddEventHandler("onResourceStart", function(s)
-    print(("^1Yo %s !"):format(s))
+-- CHAR CREATOR
+
+AddEventHandler('playerJoin', function()
+    print("aaah")
+end)
+
+CreateThread(function()
+    local player = PlayerId()
+    while true do
+        Wait(0)
+        if NetworkIsPlayerActive(player) then
+            TriggerEvent('playerJoin')
+        end
+    end
 end)
